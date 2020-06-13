1be917bb10e8e7aa8fbc3cd250e07811bd7ef261
Piyush <piyush8311@gmail.com>
2020-06-13 12:07:35 +0530

    Change ls -> ln

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ rm ~/.vimrc
 mkdir ~/.vim
 cp .vimrc ~/.vim/
 cp -R ./autoload ~/.vim/
-ls -s ~/.vim/.vimrc .vimrc
+ln -s ~/.vim/.vimrc .vimrc
 vim -c PlugInstall
 cd ~/.vim/plugged/YouCompleteMe # for ycm compilation
 ./install.py --clangd-completer
