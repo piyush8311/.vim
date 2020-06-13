fbb4baaeff41d2be0794627577316f8d544576aa
Piyush <piyush8311@gmail.com>
2020-06-13 12:23:02 +0530

    Edit install.sh file

--- install.sh
+++ install.sh
@@ -3,7 +3,9 @@ rm -rf ~/.vim
 rm ~/.vimrc
 mkdir ~/.vim
 cp .vimrc ~/.vim/
+cp install.sh ~/.vim/
 cp -R ./autoload ~/.vim/
+cd ~/
 ln -s ~/.vim/.vimrc .vimrc
 vim -c PlugInstall
 cd ~/.vim/plugged/YouCompleteMe # for ycm compilation
